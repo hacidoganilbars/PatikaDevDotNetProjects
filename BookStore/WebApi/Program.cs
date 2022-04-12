3200b928946b336de657ab2451e6f1621461895c
Haci Dogan ilbars <doganilbars80@gmail.com>
2022-04-12 05:14:16 +0300

    AutoMapper servis olarak eklendi

--- BookStore/WebApi/Program.cs
+++ BookStore/WebApi/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using WebApi.DBOperations;
 
@@ -7,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<BookStoreDBContext>(options => {
     options.UseInMemoryDatabase(databaseName:"BookStoreDB");
 });
-
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
